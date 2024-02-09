fc5fd9d8ea75290f7b3d7f3f7f52965be94b3638
Alejandra30melgarejo <Mayramelgarejo@colegioguanenta.edu.co>
2024-02-09 08:47:07 -0500

    se realizo analisis, diseño y programa en python

---
+++ AreaPerimetroCirculo.py
@@ -0,0 +1,16 @@
+# Programa para calcular el area y el perimetro deun circulo de radio R
+
+import math
+
+# input
+R = input(Ingrese el valor del radio del circulo: ")
+R = int (R)
+
+# processing
+A = math .pi * r**2
+P = 2 * math . pi * R
+
+# output 
+print("el area es : " + str (A))
+print("el perimetro es : " + str (P))
+
